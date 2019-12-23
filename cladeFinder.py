ecf5e5e4f78d47c6afd76d35e0d61e00732904dd
Hunter Provyn <hunterprovyn@gmail.com>
2019-12-23 12:22:57 +0400

    commit

--- cladeFinder.py
+++ cladeFinder.py
@@ -285,7 +285,7 @@ def findClade(positives, negatives):
         print(' unable to compute')
         
     print(recurseDownTreeUntilFirstHits("Adam", positives, hierarchy, childMap, cladeSNPs))
-    print(recurseDownTreeUntilFirstHits("J-M102", positives, hierarchy, childMap, cladeSNPs)
+    print(recurseDownTreeUntilFirstHits("J-M102", positives, hierarchy, childMap, cladeSNPs))
     print("Adam's children", childMap["Adam"])
         
 findClade(["PH1080","Z1043","Z1297","M12","M241","L283","Z1825"],["CTS11760","Z8429"])
